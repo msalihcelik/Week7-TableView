b73ce4accdf63b554b5c2571cacc2cd2977eb54a
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2021-10-26 16:49:56 +0300

    edit design

---
+++ Week7/NoteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NoteViewController.swift
+//  Week7
+//
+//  Created by Mehmet Salih ÇELİK on 26.10.2021.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+}
--- Week7/ViewController.swift
+++ Week7/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    private func configureContents() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func addNoteButtonTapped(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
 
