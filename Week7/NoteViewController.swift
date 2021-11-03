698d1751e17410e91ad07f3f52d0f5bd30ba37da
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2021-11-03 13:36:36 +0300

    add delegate

--- Week7/NoteViewController.swift
+++ Week7/NoteViewController.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 
+protocol SecondViewControllerDelegate {
+    func secondViewControllerWillPop(title: String, note: String)
+}
+
 class NoteViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var noteTextField: UITextField!
+    var selectedTitle = ""
+    var selectedNote = ""
+    var delegate: SecondViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        titleTextField.text = selectedTitle
+        noteTextField.text = selectedNote
 
     }
 
     @IBAction func saveButtonTapped(_ sender: Any) {
+        delegate?.secondViewControllerWillPop(title: titleTextField.text!, note: noteTextField.text!)
+        navigationController?.popViewController(animated: true)
     }
     
+}
+
+extension ViewController: SecondViewControllerDelegate {
+    func secondViewControllerWillPop(title: String, note: String) {
+        self.titleArray.append(title)
+        self.noteArray.append(note)
+    }
 }
--- Week7/ViewController.swift
+++ Week7/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var noteArray = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureContents()
+        titleArray.append("qwe")
+        noteArray.append("qwe")
+        titleArray.append("www")
+        noteArray.append("www")
         
     }
     private func configureContents() {
@@ -21,14 +29,55 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func addNoteButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toNoteVC", sender: nil)
+    }
+    
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//                if editingStyle == .delete {
+//                    titleArray.remove(at: indexPath.row)
+//                    noteArray.remove(at: indexPath.row)
+//                    tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+//                }
+//    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toNoteVC") {
+            let destinationVC = segue.destination as! NoteViewController
+            destinationVC.selectedTitle = ""
+            destinationVC.selectedNote = ""
+            destinationVC.delegate = self
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let editButton = UITableViewRowAction(style: .normal, title: "Edit") { rowAction, indexPath in
+            print("qwe")
+        }
+        let deleteButton = UITableViewRowAction(style: .default, title: "Delete") { rowAction, indexPath in
+            self.titleArray.remove(at: indexPath.row)
+            self.noteArray.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+        deleteButton.backgroundColor = UIColor.brown
+        return [deleteButton, editButton]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
     }
     
 }
